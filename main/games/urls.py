f26936453af4bcd79a43d1ad3e3c1ce31eb4c6fd
SaintPeter <rex.schrader@gmail.com>
2012-09-23 10:24:49 -0700

    Games:  Added "Join" and "Leave" features

--- main/games/urls.py
+++ main/games/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from games.views import GamesView, GameDetailView
 
 urlpatterns = patterns('games.views',
-    url(r'^$', GamesView.as_view()),
-    url(r'^(?P<pk>\d+)/$', GameDetailView.as_view(), name='game_detail_view'),
+    url(r'^$',                    GamesView.as_view(),        name='game_list'),
+    url(r'^(?P<pk>\d+)/$',        GameDetailView.as_view(),   name='game_detail_view'),
+    url(r'^(?P<pk>\d+)/join/$',  'game_join',                 name='game_join'),
+    url(r'^(?P<pk>\d+)/leave/$', 'game_leave',                name='game_leave'),
 )
--- main/games/views.py
+++ main/games/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from games.models import Game
+from django.shortcuts import get_object_or_404, redirect
+from games.models import Game, GameMember
 
 class GamesView(ListView):
     '''View and interact with all of the games'''
@@ -22,4 +23,37 @@ class GamesView(ListView):
 class GameDetailView(DetailView):
     template_name = 'games/game_detail.html'
     model = Game
+    
+    def get_context_data(self, **kwargs):
+        context = super(GameDetailView,self).get_context_data(**kwargs)
+        thisGame = context['game']
+        membership = thisGame.get_membership(self.request.user)
+        if membership:
+            thisGame.is_member = True
+            thisGame.joined_date = membership.date
+        else:
+            thisGame.is_member = False
+
+        return context
+    
+def game_join(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+    user = request.user
+    if GameMember.objects.filter(game=game,user=user).count() == 0:
+        print "Adding Member"
+        m = GameMember()
+        m.game = game
+        m.user = user
+        m.save()
+        
+    return redirect('game_detail_view',pk=pk)
+
+def game_leave(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+    user = request.user
+    if GameMember.objects.filter(game=game,user=user).count() == 1:
+        print "Removing Member"
+        GameMember.objects.filter(game=game,user=user).delete()
+        
+    return redirect('game_list',pk=pk)
     
